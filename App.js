29616c2e3952d93ae315535480d94a02d3d460a2
Josh Dorn <jr_dorn@yahoo.com>
2020-09-25 22:16:01 -0700

    Updated expo, added launch icon

--- App.js
+++ App.js
@@ -1,53 +1,54 @@
-import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react'
-import { firebase } from './src/firebase/config'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
-import {decode, encode} from 'base-64'
-if (!global.btoa) {  global.btoa = encode }
-if (!global.atob) { global.atob = decode }
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { firebase } from "./src/firebase/config";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginScreen, HomeScreen, RegistrationScreen } from "./src/screens";
+import { decode, encode } from "base-64";
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const usersRef = firebase.firestore().collection('users');
-    firebase.auth().onAuthStateChanged(user => {
+    const usersRef = firebase.firestore().collection("users");
+    firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         usersRef
           .doc(user.uid)
           .get()
           .then((document) => {
-            const userData = document.data()
-            setLoading(false)
-            setUser(userData)
+            const userData = document.data();
+            setLoading(false);
+            setUser(userData);
           })
           .catch((error) => {
-            setLoading(false)
+            setLoading(false);
           });
       } else {
-        setLoading(false)
+        setLoading(false);
       }
     });
   }, []);
 
   if (loading) {
-    return (
-      <></>
-    )
+    return <></>;
   }
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        { user ? (
+        {user ? (
           <Stack.Screen name="Home">
-            {props => <HomeScreen {...props} extraData={user} />}
+            {(props) => <HomeScreen {...props} extraData={user} />}
           </Stack.Screen>
         ) : (
           <>
