b4044924a0c20aae38f33c7c3c355d2349299716
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-09-30 14:45:03 -0700

    update LoginScreen.js

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -4,89 +4,6 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 import styles from "./styles";
 import { firebase } from "../../firebase/config";
 
-<<<<<<< Updated upstream
-export default function LoginScreen({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSignUpPress = () => {
-    navigation.navigate("Registration");
-  };
-
-  const onLoginPress = () => {
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((response) => {
-        const uid = response.user.uid;
-        const usersRef = firebase.firestore().collection("users");
-        usersRef
-          .doc(uid)
-          .get()
-          .then((firestoreDocument) => {
-            if (!firestoreDocument.exists) {
-              alert("User does not exist.");
-              return;
-            }
-            const user = firestoreDocument.data();
-            navigation.navigate("Home", { user });
-          })
-          .catch((error) => {
-            alert(error);
-          });
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  };
-
-  return (
-    <View style={styles.container}>
-      <KeyboardAwareScrollView
-        style={{ flex: 1, width: "100%" }}
-        keyboardShouldPersistTaps="always"
-      >
-        <Image
-          //style={styles.logo}
-          source={require("../../../assets/donorable-title.png")}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="E-mail"
-          placeholderTextColor="#aaaaaa"
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-        />
-        <TextInput
-          style={styles.input}
-          placeholderTextColor="#aaaaaa"
-          secureTextEntry
-          placeholder="Password"
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-        />
-        <TouchableOpacity
-          style={styles.loginButton}
-          onPress={() => onLoginPress()}
-        >
-          <Text style={styles.buttonTitle}>Log in</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.signupButton}
-          onPress={() => onSignUpPress()}
-        >
-          <Text style={styles.buttonTitle}>Sign up</Text>
-        </TouchableOpacity>
-      </KeyboardAwareScrollView>
-    </View>
-  );
-}
-=======
 export default function LoginScreen({navigation}) {
     const [email, setEmail] = useState('') // variable for email and password
     const [password, setPassword] = useState('')
@@ -162,4 +79,3 @@ export default function LoginScreen({navigation}) {
         </View>
     )
 }
->>>>>>> Stashed changes
