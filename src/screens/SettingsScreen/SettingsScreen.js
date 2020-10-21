a1fa45f9d75f350d39f9b4c4dd77ec0c58e31697
Josh Dorn <jr_dorn@yahoo.com>
2020-10-20 21:17:15 -0700

    Styles, added profile picture

--- src/screens/SettingsScreen/SettingsScreen.js
+++ src/screens/SettingsScreen/SettingsScreen.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import styles from "./styles";
-//import { firebase } from "../../firebase/config";
 
 export default function SettingsScreen(props) {
   const onDonePress = () => {
@@ -14,16 +13,28 @@ export default function SettingsScreen(props) {
         style={{ flex: 1, width: "100%" }}
         keyboardShouldPersistTaps="always"
       >
-        <Image source={require("../../../assets/donorable-title.png")} />
-        <Text style={styles.text}>ACCOUNT SETTINGS</Text>
-        <Text style={styles.text}>Phone Number</Text>
-        <Text style={styles.text}>Email</Text>
-        <Text style={styles.text}>Password</Text>
-        <Text style={styles.text}>Location</Text>
-        <Text style={styles.text}>Notifications</Text>
-        <Text style={styles.text}>Contact Us</Text>
-        <Text style={styles.text}>Delete Account</Text>
-        <Text style={styles.text}>Logout</Text>
+
+        <Text style={styles.header}> Settings </Text>
+
+        <Text style={styles.bar}>_____________________________</Text>
+
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>Profile picture</Text>
+          <Text style={styles.text}>Phone Number</Text>
+          <Text style={styles.text}>Email</Text>
+          <Text style={styles.text}>Password</Text>
+          <Text style={styles.text}>Location</Text>
+        </View>
+
+        <Text style={styles.bar}>_____________________________</Text>
+
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>Go anonymous</Text>
+          <Text style={styles.text}>Notifications</Text>
+          <Text style={styles.text}>Delete Account</Text>
+          <Text style={styles.text}>Logout</Text>
+        </View>
+
 
         <TouchableOpacity style={styles.button} onPress={() => onDonePress()}>
           <Text style={styles.buttonTitle}>Done</Text>
--- src/screens/SettingsScreen/styles.js
+++ src/screens/SettingsScreen/styles.js
@@ -1,34 +1,48 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  header: {
+    fontFamily: "Montserrat_400Regular",
+    fontSize: 26,
+    marginTop: 30,
+    color: "#6200ee",
+    textAlign: "center",
+  },
   text: {
     fontFamily: "Montserrat_400Regular",
+    fontSize: 20,
+    margin: 10,
+    color: "#8d9293",
+  },
+  textWrapper: {
+    marginLeft: 50,
+  },
+  bar: {
+    fontFamily: "Montserrat_400Regular",
+    fontSize: 20,
     margin: 20,
+    color: "#8d9293",
+    textAlign: "center",
   },
   container: {
     flex: 1,
     alignItems: "center",
   },
-  logo: {
-    flex: 1,
-    height: 120,
-    width: 90,
-    alignSelf: "center",
-    margin: 30,
-  },
+
   button: {
     backgroundColor: "#03dac5",
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 20,
-    height: 48,
-    borderRadius: 5,
+    marginLeft: 150,
+    marginRight: 150,
+    marginTop: 70,
+    marginBottom: 90,
+    height: 70,
+    borderRadius: 15,
     alignItems: "center",
     justifyContent: "center",
   },
   buttonTitle: {
     fontFamily: "Montserrat_400Regular",
     color: "white",
-    fontSize: 16,
+    fontSize: 26,
   },
 });
