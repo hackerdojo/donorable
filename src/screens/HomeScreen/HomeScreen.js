4cd787d707d2ba7ab8ae02b29c002f931ccd0170
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-10-21 14:06:43 -0700

    remove old/sample homescreen view code

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -13,32 +13,6 @@ import styles from "./styles";
 import { firebase } from "../../firebase/config";
 
 export default function HomeScreen(props) {
-  // const [entityText, setEntityText] = useState(""); // for what user input data
-  // const [entities, setEntities] = useState([]); // to show data after user input data
-
-  // const entityRef = firebase.firestore().collection("entities"); // firebase.firestore database to get new data
-  // const userID = props.extraData.id; // unique ID show in firebase.firestore database
-  // /* data to show what user input */
-  // useEffect(() => {
-  //   entityRef
-  //     .where("authorID", "==", userID)
-  //     .orderBy("createdAt", "desc")
-  //     .onSnapshot(
-  //       (querySnapshot) => {
-  //         const newEntities = [];
-  //         querySnapshot.forEach((doc) => {
-  //           const entity = doc.data();
-  //           entity.id = doc.id;
-  //           newEntities.push(entity);
-  //         });
-  //         setEntities(newEntities);
-  //       },
-  //       (error) => {
-  //         console.log(error);
-  //       }
-  //     );
-  // }, []);
-
   /* Navigate to settings screen */
   const onSettingsPress = () => {
     props.navigation.navigate("Settings");
@@ -49,37 +23,6 @@ export default function HomeScreen(props) {
     props.navigation.navigate("Message");
   };
 
-  /* data to get what user input */
-  // const onAddButtonPress = () => {
-  //   if (entityText && entityText.length > 0) {
-  //     const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-  //     const data = {
-  //       text: entityText,
-  //       authorID: userID,
-  //       createdAt: timestamp,
-  //     };
-  //     entityRef
-  //       .add(data)
-  //       .then((_doc) => {
-  //         setEntityText("");
-  //         Keyboard.dismiss();
-  //       })
-  //       .catch((error) => {
-  //         alert(error);
-  //       });
-  //   }
-  // };
-
-  /* variable to list what data that user can input */
-  // const renderEntity = ({ item, index }) => {
-  //   return (
-  //     <View style={styles.entityContainer}>
-  //       <Text style={styles.entityText}>
-  //         {index}. {item.text}
-  //       </Text>
-  //     </View>
-  //   );
-  // };
 
   /* View for the Home Screen */
   return (
@@ -102,30 +45,6 @@ export default function HomeScreen(props) {
         style={styles.donorableTitle}
         source={require("../../../assets/donorable-title.png")}
       />
-      {/* <View style={styles.formContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Add new item"
-          placeholderTextColor="#aaaaaa"
-          onChangeText={(text) => setEntityText(text)}
-          value={entityText}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-        />
-        <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
-          <Text style={styles.buttonText}>Add</Text>
-        </TouchableOpacity>
-      </View>
-      {entities && (
-        <View style={styles.listContainer}>
-          <FlatList
-            data={entities}
-            renderItem={renderEntity}
-            keyExtractor={(item) => item.id}
-            removeClippedSubviews={true}
-          />
-        </View>
-      )} */}
     </View>
   );
 }
