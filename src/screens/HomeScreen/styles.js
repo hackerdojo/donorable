ad5cccf7efd7b974c792059adc2d7e725716f4c5
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-10-27 11:51:48 -0700

    put all css line of code into comment and transfer inside the HomeScreen.js

--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,73 +1,118 @@
-/* CSS styles for Home Screen */
-import { StyleSheet } from 'react-native';
+// /* CSS styles for Home Screen */
+// import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
-  formContainer: {
-    flexDirection: "row",
-    height: 80,
-    marginTop: 40,
-    marginBottom: 20,
-    flex: 1,
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 30,
-    paddingRight: 30,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  input: {
-    fontFamily: "Montserrat_400Regular",
-    height: 48,
-    borderRadius: 5,
-    overflow: "hidden",
-    backgroundColor: "white",
-    paddingLeft: 16,
-    flex: 1,
-    marginRight: 5,
-  },
-  button: {
-    height: 47,
-    borderRadius: 5,
-    backgroundColor: "#6200ee",
-    width: 80,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
-    fontFamily: "Montserrat_400Regular",
-    color: "white",
-    fontSize: 16,
-  },
-  listContainer: {
-    marginTop: 20,
-    padding: 20,
-  },
-  entityContainer: {
-    marginTop: 16,
-    borderBottomColor: "#cccccc",
-    borderBottomWidth: 1,
-    paddingBottom: 16,
-  },
-  entityText: {
-    fontFamily: "Montserrat_400Regular",
-    fontSize: 20,
-    color: "#333333",
-  },
-  settingsIcon: {     // styles for settings icon
-    height: 35,
-    width: 35,
-    margin: 7,
-    alignSelf: "stretch",
-  },
-  messageIcon: {      // styles for message icon
-    marginLeft: 155,
-    marginTop: -17,
-  },
-  donorableTitle: {   // styles for donorable app logo
-    marginTop: -35,
-  }
-});
+// export default StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#ffffff',
+//     alignItems: "center",
+//     justifyContent: 'center',
+//   },
+//   settingsIcon: {     // styles for settings icon
+//     height: 35,
+//     width: 35,
+//     margin: 7,
+//     marginTop: -325,
+//     alignSelf: "stretch",
+//   },
+//   messageIcon: {      // styles for message icon
+//     marginLeft: 155,
+//     marginTop: -183,
+//   },
+//   donorableTitle: {   // styles for donorable app logo
+//     marginTop: -200,
+//   },
+//   slide: {
+//     // backgroundColor: '#ffffff'
+//   },
+//   card: {
+//     flex: 0.5,
+//     borderRadius: 28,
+//     shadowRadius: 25,
+//     shadowColor: '#000',
+//     shadowOpacity: 0.08,
+//     shadowOffset: { width: 0, height: 0 },
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#fff',
+//     marginTop: 50,
+//   },
+//   cardImage: {
+//     width: 160,
+//     flex: 0.5,
+//     resizeMode: 'contain',
+//   },
+//   swiperContainer: {
+//     flex: -0.55,
+//   },
+//   bottomContainer: {
+//     flex: -0.45,
+//   },
+//   cardDetails: {
+//     alignItems: 'center'
+//   },
+//   text: {
+//     fontFamily: 'Courier'
+//   },
+//   title: {
+//     fontSize: 24,
+//     marginBottom: 10,
+//     color: '#ffffff'
+//   },
+//   price: {
+//     color: '#0070ff',
+//     fontSize: 24,
+//     fontWeight: '400'
+//   },
+//   formContainer: {
+//     flexDirection: "row",
+//     height: 80,
+//     marginTop: 40,
+//     marginBottom: 20,
+//     flex: 1,
+//     paddingTop: 10,
+//     paddingBottom: 10,
+//     paddingLeft: 30,
+//     paddingRight: 30,
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   input: {
+//     fontFamily: "Montserrat_400Regular",
+//     height: 48,
+//     borderRadius: 5,
+//     overflow: "hidden",
+//     backgroundColor: "white",
+//     paddingLeft: 16,
+//     flex: 1,
+//     marginRight: 5,
+//   },
+//   button: {
+//     height: 47,
+//     borderRadius: 5,
+//     backgroundColor: "#6200ee",
+//     width: 80,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   buttonText: {
+//     fontFamily: "Montserrat_400Regular",
+//     color: "white",
+//     fontSize: 16,
+//   },
+//   listContainer: {
+//     marginTop: 20,
+//     padding: 20,
+//   },
+//   entityContainer: {
+//     marginTop: 16,
+//     borderBottomColor: "#cccccc",
+//     borderBottomWidth: 1,
+//     paddingBottom: 16,
+//   },
+//   entityText: {
+//     fontFamily: "Montserrat_400Regular",
+//     fontSize: 20,
+//     color: "#333333",
+//   },
+// });
