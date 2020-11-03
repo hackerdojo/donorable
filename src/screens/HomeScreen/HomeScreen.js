953eb0c5c810ce6cf646f58231a0c849ca097a21
Josh Dorn <jr_dorn@yahoo.com>
2020-11-02 23:50:31 -0800

    Pull HomeScreen-eps

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,115 +1,227 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Image,
-  FlatList,
-  Keyboard,
+  StatusBar,
   Text,
-  TextInput,
-  TouchableOpacity,
   View,
+  SafeAreaView,
+  Dimensions,
+  TouchableOpacity,
 } from "react-native";
+
 import styles from "./styles";
-import { firebase } from "../../firebase/config";
+//import { firebase } from "../../firebase/config";
+
+import data from './data';
+import Swiper from 'react-native-deck-swiper';
+import { Transitioning, Transition } from 'react-native-reanimated';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+/* new **************************/
+const { width } = Dimensions.get('window');
+const stackSize = 4;
+const ANIMATION_DURATION = 200;
+
+const transition = (
+  <Transition.Sequence>
+    <Transition.Out
+      type='slide-bottom'
+      durationMs={ANIMATION_DURATION}
+      interpolation='easeIn'
+    />
+    <Transition.Together>
+      <Transition.In
+        type='fade'
+        durationMs={ANIMATION_DURATION}
+        delayMs={ANIMATION_DURATION / 2}
+      />
+      <Transition.In
+        type='slide-bottom'
+        durationMs={ANIMATION_DURATION}
+        delayMs={ANIMATION_DURATION / 2}
+        interpolation='easeOut'
+      />
+    </Transition.Together>
+  </Transition.Sequence>
+);
+
+const swiperRef = React.createRef();
+const transitionRef = React.createRef();
+
+const Card = ({ card }) => {
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: card.image }} style={styles.cardImage} />
+    </View>
+  );
+};
+const CardDetails = ({ index }) => (
+  <View key={data[index].id} style={{ alignItems: 'center' }}>
+    <Text style={[styles.text, styles.heading]} numberOfLines={2}>
+      {data[index].name}
+    </Text>
+    <Text style={[styles.text, styles.price]}>{data[index].price}</Text>
+  </View>
+);
+/** ************************************* */
+
+
 
 export default function HomeScreen(props) {
-  const [entityText, setEntityText] = useState(""); // for what user input data
-  const [entities, setEntities] = useState([]); // to show data after user input data
-
-  const entityRef = firebase.firestore().collection("entities"); // firebase.firestore database to get new data
-  const userID = props.extraData.id; // unique ID show in firebase.firestore database
-  /* data to show what user input */
-  useEffect(() => {
-    entityRef
-      .where("authorID", "==", userID)
-      .orderBy("createdAt", "desc")
-      .onSnapshot(
-        (querySnapshot) => {
-          const newEntities = [];
-          querySnapshot.forEach((doc) => {
-            const entity = doc.data();
-            entity.id = doc.id;
-            newEntities.push(entity);
-          });
-          setEntities(newEntities);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  }, []);
-
+  
+    
   /* Navigate to settings screen */
   const onSettingsPress = () => {
     props.navigation.navigate("Settings");
   };
 
-  /* data to get what user input */
-  const onAddButtonPress = () => {
-    if (entityText && entityText.length > 0) {
-      const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-      const data = {
-        text: entityText,
-        authorID: userID,
-        createdAt: timestamp,
-      };
-      entityRef
-        .add(data)
-        .then((_doc) => {
-          setEntityText("");
-          Keyboard.dismiss();
-        })
-        .catch((error) => {
-          alert(error);
-        });
-    }
+  /** new **********************/
+  const [index, setIndex] = React.useState(0);
+  const onSwiped = () => {
+    transitionRef.current.animateNextTransition();
+    setIndex((index + 1) % data.length);
   };
 
-  /* variable to list what data that user can input */
-  const renderEntity = ({ item, index }) => {
-    return (
-      <View style={styles.entityContainer}>
-        <Text style={styles.entityText}>
-          {index}. {item.text}
-        </Text>
-      </View>
-    );
-  };
+    /* Navigate to message screen, NEED TO IMPLEMENT*/
+    const onMessagePress = () => {
+      props.navigation.navigate("Message");
+    };
+
+ /**************************** */
+
 
   /* View for the Home Screen */
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.settingsIcon} onPress={onSettingsPress}>
-        <Image
-          style={styles.settingsIcon}
-          source={require("../../../assets/settings-icon.png")}
-        />
-      </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <MaterialCommunityIcons
+      name='crop-square'
+      size={width}
+      style={{
+        opacity: 0.05,
+        transform: [{ rotate: '45deg' }, { scale: 1.6 }],
+        position: 'absolute',
+        left: -15,
+        top: 140
+      }}
+      />
+      <StatusBar hidden={true} />
 
-      <Image source={require("../../../assets/donorable-title.png")} />
-      <View style={styles.formContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Add new item"
-          placeholderTextColor="#aaaaaa"
-          onChangeText={(text) => setEntityText(text)}
-          value={entityText}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-        />
-        <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
-          <Text style={styles.buttonText}>Add</Text>
-        </TouchableOpacity>
-      </View>
-      {entities && (
-        <View style={styles.listContainer}>
-          <FlatList
-            data={entities}
-            renderItem={renderEntity}
-            keyExtractor={(item) => item.id}
-            removeClippedSubviews={true}
-          />
-        </View>
-      )}
+      <View style={styles.swiperContainer}>
+        {/* Profile Card Swiper */}
+        <Swiper
+        ref={swiperRef}
+        cards={data}
+        cardIndex={index}
+        renderCard={card => <Card card={card} />}
+        infinite
+        backgroundColor={'transparent'}
+        onSwiped={onSwiped}
+        onTapCard={() => swiperRef.current.swipeLeft()}
+        cardVerticalMargin={50}
+        stackSize={stackSize}
+        stackScale={10}
+        stackSeparation={14}
+        animateOverlayLabelsOpacity
+        animateCardOpacity
+        disableTopSwipe
+        disableBottomSwipe
+        overlayLabels={{
+          left: {
+            title: 'NOPE',
+            style: {
+              label: {
+                backgroundColor: "red",
+                borderColor: "red",
+                color: "red",
+                borderWidth: 1,
+                fontSize: 24
+              },
+              wrapper: {
+                flexDirection: 'column',
+                alignItems: 'flex-end',
+                justifyContent: 'flex-start',
+                marginTop: 20,
+                marginLeft: -20
+              }
+            }
+          },
+          right: {
+            title: 'LIKE',
+            style: {
+              label: {
+                backgroundColor: "blue",
+                borderColor: "blue",
+                color: "blue",
+                borderWidth: 1,
+                fontSize: 24
+              },
+              wrapper: {
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                justifyContent: 'flex-start',
+                marginTop: 20,
+                marginLeft: 20
+              }
+            }
+          }
+        }}
+      />
     </View>
-  );
+
+    {/* Bottom Container Main */}
+    <View style={styles.bottomContainer}>
+      {/* Card Details or Description */}
+      <Transitioning.View
+        ref={transitionRef}
+        transition={transition}
+        style={styles.bottomContainerMeta}
+      >
+      <CardDetails index={index} />
+      </Transitioning.View>
+
+      {/* Bottom Container Buttons */}
+      <View style={styles.bottomContainerButtons}>
+        {/* X red button */}
+        <MaterialCommunityIcons.Button
+          name='close'
+          size={90}
+          backgroundColor='transparent'
+          underlayColor='transparent'
+          activeOpacity={0.3}
+          color="red"
+          onPress={() => swiperRef.current.swipeLeft()}
+        />
+        {/* O blue button */}
+        <MaterialCommunityIcons.Button
+          name='circle-outline'
+          size={90}
+          backgroundColor='transparent'
+          underlayColor='transparent'
+          activeOpacity={0.3}
+          color="blue"
+          onPress={() => swiperRef.current.swipeRight()}
+        />
+      </View>
+
+      {/* Header Icon & Image */}
+      <View style={styles.header}>
+        {/* Setting Icon Press */}
+        <TouchableOpacity
+          style={styles.settingsIcon} onPress={onSettingsPress}>
+          <Image source={require("../../../assets/settings-icon.png")} />
+        </TouchableOpacity>
+          {/* Message Icon on Press */}
+        <TouchableOpacity
+          style={styles.messageIcon} onPress={onMessagePress}>
+          <Image source={require("../../../assets/message.png")} />
+        </TouchableOpacity>
+          {/* Donorable App Logo */}
+        <Image
+          style={styles.donorableTitle}
+          source={require("../../../assets/donorable-title.png")}
+        />
+      </View>
+    </View>
+  </SafeAreaView>
+);
 }
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,66 +1,98 @@
 /* CSS styles for Home Screen */
 import { StyleSheet } from 'react-native';
 
+
+
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
-  formContainer: {
-    flexDirection: "row",
-    height: 80,
-    marginTop: 40,
-    marginBottom: 20,
-    flex: 1,
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 30,
-    paddingRight: 30,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  input: {
-    fontFamily: "Montserrat_400Regular",
-    height: 48,
-    borderRadius: 5,
-    overflow: "hidden",
-    backgroundColor: "white",
-    paddingLeft: 16,
-    flex: 1,
-    marginRight: 5,
-  },
-  button: {
-    height: 47,
-    borderRadius: 5,
-    backgroundColor: "#6200ee",
-    width: 80,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
-    fontFamily: "Montserrat_400Regular",
-    color: "white",
-    fontSize: 16,
-  },
-  listContainer: {
-    marginTop: 20,
-    padding: 20,
-  },
-  entityContainer: {
-    marginTop: 16,
-    borderBottomColor: "#cccccc",
-    borderBottomWidth: 1,
-    paddingBottom: 16,
-  },
-  entityText: {
-    fontFamily: "Montserrat_400Regular",
-    fontSize: 20,
-    color: "#333333",
-  },
-  settingsIcon: {
-    height: 35,
-    width: 35,
-    margin: 10,
-    alignSelf: "stretch",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+      },
+      swiperContainer: {
+        flex: 0.55,
+        top: 55,
+      },
+      bottomContainer: {
+        flex: 0.45,
+        justifyContent: 'space-evenly'
+      },
+      bottomContainerMeta: {
+        alignContent: 'flex-end',
+        alignItems: 'center'
+      },
+      bottomContainerButtons: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        top: 65,
+      },
+      header: {
+        flex: 0.5,
+        // justifyContent: 'space-between'
+      },
+      cardImage: {
+        width: 160,
+        flex: 1,
+        resizeMode: 'contain'
+      },
+      card: {
+        flex: 0.45,
+        borderRadius: 8,
+        shadowRadius: 25,
+        shadowColor: "black",
+        shadowOpacity: 0.08,
+        shadowOffset: { width: 0, height: 0 },
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "white",
+      },
+      text: {
+        textAlign: 'center',
+        fontSize: 50,
+        backgroundColor: 'transparent'
+      },
+      done: {
+        textAlign: 'center',
+        fontSize: 30,
+        color: "white",
+        backgroundColor: 'transparent'
+      },
+      text: {
+        fontFamily: 'Courier'
+      },
+      heading: {
+        fontSize: 24,
+        marginBottom: 10,
+        color: "gray",
+        top: 65,
+      },
+      price: {
+        color: "blue",
+        fontSize: 15,
+        fontWeight: '500',
+        top: 60,
+      },
+      settingsIcon: {
+        position: 'absolute',
+        left: 5,
+        right: 5,
+        top: -599,
+        bottom: 0,
+        height: 35,
+        width: 5,
+        margin: 7,
+      },
+      messageIcon: {
+        position: 'absolute',
+        left: 320,
+        right: 5,
+        top: -589,
+        bottom: 0,
+      },
+      donorableTitle: {
+        position: 'absolute',
+        left: 95,
+        right: 5,
+        top: -608,
+        bottom: 0,
+      },
 });
