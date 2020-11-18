284ec06d8be16f6658c10b3c8116c0e06fb82a4d
Eliezer Santiago <santiagoeliezerperez@gmail.com>
2020-11-17 18:53:36 -0800

    Made some changes in MessageScreen to return data from user

--- src/screens/MessageScreen/MessageScreen.js
+++ src/screens/MessageScreen/MessageScreen.js
@@ -19,64 +19,64 @@ export default function MessageScreen(props) {
   /* View for the Message screen */
   return (
     <View style={styles.container}>
-      <KeyboardAwareScrollView
+      {/* <KeyboardAwareScrollView
         style={{ flex: 1, width: "100%" }}
         keyboardShouldPersistTaps="always"
       >
 
 
-        
+
         <Text style={styles.label}>Messages</Text>
 
-        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>  
+        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>
 
         <Image
           source={require("../../../assets/card_hackerdojo.png")}
           style={styles.smallCard}
-        />  
+        />
         <Text style={styles.nonprofitTitle}>Hacker Dojo</Text>
         <Text style={styles.msgPreview}>Hi</Text>
 
-        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>  
+        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>
 
         <Image
           source={require("../../../assets/card_handson.png")}
           style={styles.smallCard}
-        />  
+        />
         <Text style={styles.nonprofitTitle}>HandsOn</Text>
         <Text style={styles.msgPreview}>Hey!</Text>
 
-        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>  
+        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>
 
 
         <Image
           source={require("../../../assets/card_lacasa.png")}
           style={styles.smallCard}
-        />  
+        />
         <Text style={styles.nonprofitTitle}>La Casa de las Madres</Text>
         <Text style={styles.msgPreview}>How's it going?</Text>
 
-        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>  
+        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>
 
         <Image
           source={require("../../../assets/card_aspire.png")}
           style={styles.smallCard}
-        />  
+        />
         <Text style={styles.nonprofitTitle}>Aspire</Text>
         <Text style={styles.msgPreview}>What are the most...</Text>
 
-        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>  
+        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>
 
         <Image
           source={require("../../../assets/card_stand.png")}
           style={styles.smallCard}
-        />  
+        />
         <Text style={styles.nonprofitTitle}>STAND!</Text>
         <Text style={styles.msgPreview}>How do you keep...</Text>
 
-        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>  
+        <Image style={styles.divBar} source={require("../../../assets/div-bar.png")}/>
 
-        
+
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={styles.backButton}
@@ -87,7 +87,7 @@ export default function MessageScreen(props) {
         </View>
 
 
-      </KeyboardAwareScrollView>
+      </KeyboardAwareScrollView> */}
     </View>
   );
 }
