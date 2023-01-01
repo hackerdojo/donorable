3ab3e45bd62b8490bb757617afdd7938513418a1
Louis Roehrs <lroehrs@gmail.com>
2023-01-01 03:22:39 -0800

    Updated QuickDonateScreen.js with new styles.

--- src/screens/QuickDonateScreen/QuickDonateScreen.js
+++ src/screens/QuickDonateScreen/QuickDonateScreen.js
@@ -4,6 +4,9 @@ import FormTextInput from "../../components/FormTextInput";
 import FormButton from "../../components/FormButton";
 import KeyboardAvoidingView from "react-native/Libraries/Components/Keyboard/KeyboardAvoidingView";
 import styleguide from "../../../styles/styleguide";
+import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
+import theme from "../../../styles/theme.style";
+import ImageMask from "../../components/ImageMask";
 
 
 export default function QuickDonateScreen({navigation, route}) {
@@ -22,22 +25,27 @@ export default function QuickDonateScreen({navigation, route}) {
   /* View for the QuickDonateScreen */
   return (
     <View style={styles.screen}>
-      <KeyboardAvoidingView
-        style={styles.containerKeyboardAvoidingView}
+      <KeyboardAwareScrollView
+        style={styles.fullWidth}
         keyboardShouldPersistTaps="always"
       >
-        <View style={styles.horizontalContainer}>
-          <Image
-            source={{ uri:params.image }}
-            resizeMode={"contain"}
-            style={styles.square100}
+
+          <Image resizeMode={"contain"}
+                 style={styles.fullWidth}
+            source={{uri:params.image}}
+            borderColor={theme.PRIMARY_COLOR}
+            size={100}
+            radius={5}
+            height={100}
+            borderWidth={3}
+            backgroundColor={"transparent"}
           />
+        <Text/>
           <View>
             <Text style={styles.textCenteredP2}>How much would</Text>
-            <Text style={styles.textCenteredP2}>like to donate?</Text>
-            <Text style={styles.textCenteredP2}>{ params.name }</Text>
+            <Text style={styles.textCenteredP2}>you like to donate to</Text>
+            <Text style={styles.textCenteredP2}>{ params.name }?</Text>
           </View>
-        </View>
 
         <FormTextInput
           styles={styles}
@@ -51,13 +59,16 @@ export default function QuickDonateScreen({navigation, route}) {
           <FormButton
             styles={styles}
             label={"$5"}
-            buttonStyle={{...styles.buttonSecondary, width:"25%"}}
+            width={"25%"}
+            size={"large"}
+            buttonStyle={"Secondary"}
             onPress={()=>setAmount("5")}
           />
           <FormButton
             styles={styles}
             label={"$10"}
-            buttonStyle={{...styles.buttonSecondary, width:"25%"}}
+            size={"large"}
+            buttonStyle={"Secondary"}
             width={"25%"}
             onPress={()=>setAmount("10")}
           />
@@ -66,26 +77,30 @@ export default function QuickDonateScreen({navigation, route}) {
           <FormButton
             styles={styles}
             label={"$15"}
-            buttonStyle={{...styles.buttonSecondary, width:"25%"}}
+            size={"large"}
+            width={"25%"}
+            buttonStyle={"Secondary"}
             onPress={()=>setAmount("15")}
           />
           <FormButton
             styles={styles}
             label={"$20"}
-            buttonStyle={{...styles.buttonSecondary, width:"25%"}}
+            size={"large"}
+            width={"25%"}
+            buttonStyle={"Secondary"}
             onPress={()=>setAmount("20")}
           />
 
         </View>
         <FormButton
           styles={styles}
-          buttonStyle={styles.buttonPrimary}
+          buttonStyle={"Primary"}
           onPress={onReturnPress}
           label={"Donate"}
           ></FormButton>
         <Text> </Text>
 
-      </KeyboardAvoidingView>
+      </KeyboardAwareScrollView>
     </View>
   );
 }
