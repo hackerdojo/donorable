f1c66677816d6b6cdf0a868051d1bf8b458753a9
Louis Roehrs <lroehrs@gmail.com>
2023-02-03 23:58:06 -0800

    Populated favorites screen

--- src/screens/FavoritesScreen/FavoritesScreen.js
+++ src/screens/FavoritesScreen/FavoritesScreen.js
@@ -8,12 +8,16 @@ import firebase from "../../firebase/config";
 import {PrincipalContext} from "../../contexts/PrincipalContext";
 import {NativeText} from "react-native/Libraries/Text/TextNativeComponent";
 import theme from "../../../styles/theme.style";
+import ImageMask from "../../components/ImageMask";
 
+// mock
+import data from '../HomeScreen/data';
 
 export default function FavoritesScreen({navigation, route}) {
 
   const styles = StyleSheet.create(styleguide);
 
+  const {user, updateUser} = useContext(PrincipalContext);
 
 
   return (
@@ -25,13 +29,26 @@ export default function FavoritesScreen({navigation, route}) {
         keyboardShouldPersistTaps="always"
       >
 
+        <Text numberOfLines={10}>
+          Favorites
+        </Text>
 
-
-          <Text numberOfLines={10}>
-            Favorites
-          </Text>
-
-
+        { user.favorites.map( (fav) =>
+          <View style={styles.horizontalContainer}>
+            <ImageMask
+              source={{uri:data.filter( thing => thing.id === fav)[0].image}}
+              borderColor={theme.PRIMARY_COLOR}
+              size={100}
+              radius={5}
+              borderWidth={3}
+              backgroundColor={"transparent"}
+            />
+            <View>
+              <Text style={styles.textCenteredP2}>{data.filter( thing => thing.id === fav)[0].name}</Text>
+            </View>
+          </View>
+        )
+        }
 
       </KeyboardAwareScrollView>
     </View>
